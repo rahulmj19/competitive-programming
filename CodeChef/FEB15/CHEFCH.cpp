fb5c523b20f3ef880b56c97a46aebcf3e93d0c31
Haseeb ur Rahman <me@haseeb.co>
2015-02-11 19:36:34 +0530

    .

--- CodeChef/FEB15/CHEFCH.cpp
+++ CodeChef/FEB15/CHEFCH.cpp
@@ -2,21 +2,33 @@
 #include <iomanip>
 #include <algorithm>
 #include <cmath>
-#include <map>
+#include <vector>
 using namespace std;
-
 int main() {
-    int T, N;
-    long long A;
-    cin >> T;
-    while(T--) {
-        map<int, int> mymap;
-        cin >> N;
-        for(int i=0; i<N; ++i) {
-            cin >> A;
-            mymap[A]++;
-        }
-        for(map<int, int>::iterator it = mymap.begin(); it != mymap.end(); ++it) cout << it->first << " " << it->second << "\n";
-        cout << "--------\n";
+  int T;
+  string S;
+  cin >> T;
+    for(int j=0; j<T; ++j) {
+    cin >> S;
+    int corrections1 = 0,
+        corrections2 = 0,
+        corrections,
+        len = S.length();
+    string ideal1, ideal2;
+    for(int i=0; i<len; ++i) {
+    ideal1.append("-+");
+    ideal2.append("+-");
     }
+    if(len == 1) corrections = corrections1 = 0;
+    else {
+      for(int i=0; i<len; ++i) {
+        if(S[i] != ideal1[i]) ++corrections1;
+        if(S[i] != ideal2[i]) ++corrections2;
+      }
+      if (corrections1 < corrections2) corrections = corrections1;
+      else corrections = corrections2;
+    }
+    cout << corrections << "\n";
+  }
+	return 0;
 }
