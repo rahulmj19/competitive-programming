e08e8803e5bf3544a15d9ce48372fa757b9cfebb
Haseeb ur Rahman <me@haseeb.co>
2015-02-11 19:13:10 +0530

    codechef

---
+++ CodeChef/FEB15/CHEFEQ.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+using namespace std;
+//---+-+-+++
+int main() {
+  int T;
+  string S;
+  cin >> T;
+    for(int j=0; j<T; ++j) {
+    cin >> S;
+    int corrections1 = 0, corrections2 = 0;
+    int corrections;
+    int len = S.length();
+    string ideal1, ideal2;
+    for(int i=0; i<len; ++i) {
+    ideal1.append("-");
+    ideal1.append("+");
+    ideal2.append("+");
+    ideal2.append("-");
+    }
+    if(len == 1) corrections = corrections1 = 0;
+    else {
+      for(int i=0; i<len; ++i) {
+        if(S[i] != ideal1[i]) ++corrections1;
+        if(S[i] != ideal2[i]) ++corrections2;
+      }
+      if (corrections1 < corrections2) corrections = corrections1;
+      else corrections = corrections2;
+    }
+    cout << corrections << "\n";
+  }
+	return 0;
+}
---
+++ Timus/1002.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+using namespace std;
+/*
+7325189087
+5
+it
+your
+reality
+real
+our
+4294967296
+5
+it
+your
+reality
+real
+our
+-1
+*/
+
+bool check(int n, char character) {
+
+}
+int main() {
+    string num, word;
+    vector<string> v;
+    v.push_back('lol');
+    int c;
+    cout << v[0];
+    cin >> num >> c;
+    /*for(int i = 0; i < c; ++i) {
+        while(cin.get() =! -1) {
+            cout << 'lol';
+        }
+        //v.push_back(word);
+    }*/
+}
