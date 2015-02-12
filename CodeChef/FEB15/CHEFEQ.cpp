74f45caddc485a8995969e353b246c23ac6a36d9
Haseeb ur Rahman <me@haseeb.co>
2015-02-12 13:40:54 +0530

    chefeq

--- CodeChef/FEB15/CHEFEQ.cpp
+++ CodeChef/FEB15/CHEFEQ.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cmath>
 #include <map>
+#include <vector>
 using namespace std;
 
 int main() {
@@ -10,13 +11,17 @@ int main() {
     long long A;
     cin >> T;
     while(T--) {
+        int corrections=0;
         map<int, int> mymap;
+        vector<int> arr;
         cin >> N;
         for(int i=0; i<N; ++i) {
             cin >> A;
             mymap[A]++;
         }
-        for(map<int, int>::iterator it = mymap.begin(); it != mymap.end(); ++it) cout << it->first << " " << it->second << "\n";
-        cout << "--------\n";
+        for(map<int, int>::iterator it = mymap.begin(); it != mymap.end(); ++it) arr.push_back(it->second);
+        sort(arr.rbegin(), arr.rend());
+        for(int i=1; i<arr.size(); ++i) corrections += arr[i];
+        cout << corrections << "\n" ;
     }
 }
